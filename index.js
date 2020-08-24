7ed201e8764d22f93463c84cd6a11083e67d5f30
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-24 21:30:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let findMatching = function (arrayString, name) {
   return arrayString.filter((e) => e.toLowerCase() == name.toLowerCase());
 };
+
+
+let fuzzyMatch = function(arrayString, firstLetter){
+  return arrayString.filter((e)=> e.charAt(0).toLowerCase() === firstLetter.toLowerCase());
+}
